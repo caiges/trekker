4259eff406a256c3ad3f197878e9b4578ccbc864
caiges <caigesn@gmail.com>
2021-09-14 21:55:19 -0700

    Add test for next_migration_prefix

--- src/sql.rs
+++ src/sql.rs
@@ -1,4 +1,5 @@
 use glob::{glob, Paths, PatternError};
+use std::num::ParseIntError;
 
 pub fn get_files(path: &str) -> Result<Paths, PatternError> {
     glob(&format!("{}/*.sql", path))
@@ -31,6 +32,11 @@ fn last_migration_prefix(paths: Paths) -> Option<String> {
     }
 }
 
+fn next_migration_prefix(last_prefix: String) -> Result<String, ParseIntError> {
+    let last_prefix_int = last_prefix.parse::<i32>()?;
+    Ok(format!("{:0>3}", last_prefix_int + 1).to_string())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,7 +45,16 @@ mod tests {
     fn test_last_migration_prefix() {
         let paths = glob("test/sql_files/*.sql").expect("could not get test sql files");
         let last_migration_prefix = last_migration_prefix(paths);
-        println!("{:?}", last_migration_prefix);
         assert!(last_migration_prefix.unwrap() == "01");
+    }
+
+    #[test]
+    fn test_next_migration_prefix() {
+        let paths = glob("test/sql_files/*.sql").expect("could not get test sql files");
+        let last_migration_prefix = last_migration_prefix(paths).unwrap();
+        let next_migration_prefix = next_migration_prefix(last_migration_prefix).unwrap();
+
+        println!("{:?}", next_migration_prefix);
+        assert!(next_migration_prefix == "002");
     }
 }
